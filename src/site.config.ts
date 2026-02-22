d95bdbfe0e6f6bd44e5572dc9df29c647299e334
Mashbean <mashbean@gmail.com>
2026-02-22 12:28:34 +0800

    chore(site): update site description copy

--- src/site.config.ts
+++ src/site.config.ts
@@ -1,5 +1,5 @@
 export const SITE_TITLE = "Mashbean Blog";
-export const SITE_DESCRIPTION = "公共網路、數位治理、文化科技與公民社會的筆記。";
+export const SITE_DESCRIPTION = "站在單向鏡後方觀看世界應對加速科技的方式。";
 export const SITE_LANG = "zh-TW";
 export const SITE_TIMEZONE = "Asia/Taipei";
 export const DEFAULT_AUTHOR = "Mashbean";
