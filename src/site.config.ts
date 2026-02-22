c767e98ec1c359b5406b30543b5e41abc68920da
Mashbean <mashbean@gmail.com>
2026-02-22 22:39:43 +0800

    chore(og): switch default share image to updated og-default svg

--- src/site.config.ts
+++ src/site.config.ts
@@ -3,4 +3,4 @@ export const SITE_DESCRIPTION = "站在單向鏡後方觀看世界應對加速�
 export const SITE_LANG = "zh-TW";
 export const SITE_TIMEZONE = "Asia/Taipei";
 export const DEFAULT_AUTHOR = "Mashbean";
-export const DEFAULT_OG_IMAGE = "images/og-image.png";
+export const DEFAULT_OG_IMAGE = "images/og-default.svg";
